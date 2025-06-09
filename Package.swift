0aa4f17132ce214657a076a40d3e33febe825162
Vladimir Burdukov <chippcheg@gmail.com>
2025-06-09 11:05:56 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BuildPluginUpdater",
+    platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-package-manager", branch: "swift-6.0-RELEASE"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.2.2"))
+    ],
+    targets: [
+        .executableTarget(name: "BuildPluginUpdater", dependencies: [
+            .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            .product(name: "SwiftPMDataModel-auto", package: "swift-package-manager")
+        ]),
+    ]
+)
